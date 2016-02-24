1ee9731c69854ee9b90d17e873d706322a17b9d1
eivanphils <eivanphils@gmail.com>
2016-02-24 14:13:11 -0430

    configuracion de socket mysql en unix

--- config/database.php
+++ config/database.php
@@ -55,9 +55,10 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'unix_socket' => '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock',
+            'database'  => env('DB_DATABASE', 'cinema'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', 'root'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
