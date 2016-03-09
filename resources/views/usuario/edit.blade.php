d1b84342b5efe012b594da559884c6a6112e2834
eivanphils <eivanphils@gmail.com>
2016-03-09 16:00:02 -0430

    Eliminar usuario con metodo destroy

--- resources/views/usuario/edit.blade.php
+++ resources/views/usuario/edit.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
     {!!Form::model($user,['route'=>['usuario.update', $user->id], 'method'=>'PUT'])!!}
-        @include('usuario.forms.usr')
-        {!!Form::submit('Editar', ['class'=>'btn btn-success'])!!}
+    @include('usuario.forms.usr')
+    {!!Form::submit('Editar', ['class'=>'btn btn-success'])!!}
+    {!!Form::close()!!}
+
+    {!!Form::open(['route'=>['usuario.destroy', $user->id], 'method'=>'DELETE'])!!}
+    {!!Form::submit('Eliminar', ['class'=>'btn btn-danger'])!!}
     {!!Form::close()!!}
 @stop
