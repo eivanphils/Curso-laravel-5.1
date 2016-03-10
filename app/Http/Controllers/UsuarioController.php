2a6127e80f6d893cc4cab377effaa1b0dc3a2bf9
eivanphils <eivanphils@gmail.com>
2016-03-10 15:41:32 -0430

    Implementando el borrado logico 'softdelete' en la tabla usuario

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -99,7 +99,8 @@ class UsuarioController extends Controller
      */
     public function destroy($id)
     {
-        User::destroy($id);
+        $user = User::find($id);
+        $user->delete();
         Session::flash('message', 'Usuario eliminado');
         return Redirect::to('/usuario');
     }
